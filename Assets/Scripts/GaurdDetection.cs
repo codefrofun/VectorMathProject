525f5d08d8dbe65b65775aea3ac837b9a33a3113
codefrofun <blackpinkaricat@gmail.com>
2024-12-02 13:19:54 -0400

    Updated gaurd code

--- Assets/Scripts/GaurdDetection.cs
+++ Assets/Scripts/GaurdDetection.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GaurdDetection : MonoBehaviour
@@ -7,6 +9,18 @@ public class GaurdDetection : MonoBehaviour
 
     private void Update()
     {
-        
+        float alignment = Vector2.Dot(vectorA, vectorB);
+
+        float dotProduct = Vector3.Dot(transform.forward, (targetPosition - transform.position).normalized);
+
+        if (dotProduct > 0.5f)
+        {
+            Debug.Log("Target is nearby!");
+        }
+    }
+
+    public bool InFieldOfView(Transform target, float viewAngle)   // why view angle?
+    {
+
     }
 }
